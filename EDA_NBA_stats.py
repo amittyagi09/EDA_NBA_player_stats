8e2cee5ccf468478413184cff87d765da81366c0
amittyagi09 <tyagiamit3141@gmail.com>
2025-02-21 23:23:18 +0530

    Update EDA_NBA_stats.py

--- EDA_NBA_stats.py
+++ EDA_NBA_stats.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 st.title("NBA Player Stats Data")
 st.write("This webapp perform web scrapping of NBA players stats data")
-st.write("**Python Libraries:** base64, pandas, matplotlib")
+st.write("**Python Libraries:** pandas, matplotlib")
 st.write("**Data Source:**[basketball-reference.com](https://www.basketball-reference.com)")
 st.write("***")
 
